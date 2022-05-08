31c9e9825065b232ec7e6c1e11b512be974e0d6a
lawmans <68381381+lawmans@users.noreply.github.com>
2022-05-08 22:40:10 +0100

    Initial commit

---
+++ Data.py
@@ -0,0 +1,7 @@
+import pandas as pd
+path ="C:\Users\LAWMANS\Desktop\PYTHON\QA Postgresql.txt"
+file = pd.read_txt(open(path))
+print(file)
+
+
+
---
+++ pandas.py
@@ -0,0 +1,3 @@
+import  pandas as pd
+df.pd.DataFrame({'Yes':[67,23,56],'No':[45,20,32]})
+df
